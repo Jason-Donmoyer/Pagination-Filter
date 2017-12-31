562ad72acc83cbda9a487a6ff96b31760f7d6036
theDuke523 <j.donmoyer523@gmail.com>
2017-12-31 13:13:52 -0500

    added comments and functionality to buttons

--- js/app.js
+++ js/app.js
@@ -1,47 +1,51 @@
-let pageNumber = 1;
-//let studentList = [];
+
+// variables to seperate the students into a collection
 const studentList = document.querySelector('ul');
 const studentInfo = studentList.children;
 const studentsPerPage = 10;
+// variables to create page buttons
 const divButton = document.querySelector('.pagination');
 const newPageButton = divButton.querySelector('ul');
 
+// function to get the number of pages required
 function numberOfPages() {
   let numberOfPages = Math.ceil(studentInfo.length / studentsPerPage);
   return numberOfPages;
 }
-
+// function to load the initial page of 10 students
 function loadPage() {
-    // first hide all students on the page
-    //studentList.style.display = 'none';
-    // Then loop through all students in our student list argument
     for (i = 0; i < studentInfo.length;i += 1) {
-       // if student should be on this page number
        if (i < studentsPerPage) {
-        // show the student
         studentInfo[i].style.display = '';
        } else {
         studentInfo[i].style.display = 'none';
        }  
-    }  
+    }
+   // the appendPageLinks function adds the buttons to the page   
     appendPageLinks();  
  }
 
+// function to show the page corresponding to the button clicked
  function showNewPage () {
-  newPageButton.addEventListener('click', (e) {
-
-  } )
+  newPageButton.addEventListener('click', function(e) {
+    let buttonNum = parseInt(e.target.textContent);
+    // used to test the parseInt method 
+    console.log(buttonNum);
+    let maxPerPage = buttonNum * 10;
+    let minPerPage = maxPerPage - 10;
+    for (let i = 0; i < studentInfo.length; i++) {
+        if (i >= minPerPage && i < maxPerPage) {
+            studentInfo[i].style.display = '';
+        }  else {
+            studentInfo[i].style.display = 'none';
+        }
+      }     
+  });
  }
 
-
-
-
+// function to create the page buttons
 function appendPageLinks() {
-    // determine how many pages for this student list 
-    // create a page link section
-    
-    // “for” every page
-    for (let i = 0; i <= numberOfPages(); i += 1) {
+    for (let i = 1; i <= numberOfPages(); i += 1) {
       let newPage = document.createElement('li');
       let newPageLink = document.createElement('a');
       newPageLink.className = 'active';
@@ -49,14 +53,9 @@ function appendPageLinks() {
       newPageLink.textContent = i;
       newPageButton.appendChild(newPage);
       newPage.appendChild(newPageLink);
-
+      // add the functionality to the buttons
+      showNewPage();
     }
-        // add a page link to the page link section
-    // remove the old page link section from the site
-    // append our new page link section to the site
-    // define what happens when you click a link
-        // Use the showPage function to display the page for the link clicked
-        // mark that link as “active”
 }
 
 
